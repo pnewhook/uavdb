c0cf4d372dd274452053f8223f7427cbce5f2f60
Peter Newhook <peter.newhook@gmail.com>
2015-04-02 17:59:47 -0400

    first attempt at appveyor build

---
+++ build.ps1
@@ -0,0 +1,7 @@
+﻿$kDirectory = Join-Path -ChildPath .k\ -Path $env:USERPROFILE
+$env:Path += ";" + $kDirectory + "\bin"
+k upgrade
+$runtime = Get-ChildItem -Directory -Path C:\Users\pnewhook\.k\runtimes | Select-Object -First 1 
+$env:Path += ";" + $runtime.FullName
+kpm restore .\src
+kpm build .\src
