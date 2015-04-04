33db86142660a539e64e6080d965fa0c0c20777f
Peter Newhook <peter.newhook@gmail.com>
2015-04-04 10:16:53 -0400

    build script formatting trouble

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-$kDirectory = Join-Path -ChildPath .k\ -Path $env:USERPROFILE
-$env:Path += ";" + $kDirectory + "bin"
-kvm upgrade
-kpm restore .\src
-kpm build .\src
+    $kDirectory = Join-Path -ChildPath .k\ -Path $env:USERPROFILE
+    $env:Path += ";" + $kDirectory + "bin"
+    kvm upgrade
+    kpm restore .\src
+    kpm build .\src
