b3c3724c0bc0801790467d0117b1fbe340dc9442
Peter Newhook <peter.newhook@gmail.com>
2015-04-04 10:55:20 -0400

    switching to gulp

---
+++ src/gulpfile.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var gulp = require("gulp");
+var bower = require("gulp-bower");
+var del = require("del");
+
+gulp.task("bower", function () {
+    return bower()
+        .pipe(gulp.dest("wwwroot/lib"));
+});
+
+gulp.task("clean", function (cb) {
+    del("./wwwroot/lib", cb);
+});
+          
+gulp.task("default", ["clean", "bower"]);
